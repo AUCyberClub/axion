4f8b0a12837989f7bde52d280622899926bdcdfc
Behçet Şentürk <behcet.senturk@protonmail.com>
2018-03-11 11:57:59 +0300

    Some improvements in vigenere decoder

--- imports/vigenere_decoder.py
+++ imports/vigenere_decoder.py
@@ -20,15 +20,34 @@ logo = ("""
 /_/   \_\/_/\_\___\___/|_| \_|    /_/   \_\___/ \____\____|
         """)
 
-def decode_vigenere(vigenere_msg, key):
-    key_length = len(key)
-    key_as_int = [ord(i) for i in key]
-    vigenere_msg_int = [ord(i) for i in vigenere_msg]
-    text_msg = ''
-    for i in range(len(vigenere_msg_int)):
-        value = (vigenere_msg_int[i] - key_as_int[i % key_length]) % 26
-        text_msg += chr(value + 65)
-    return text_msg
+# Vigenere Cipher (Polyalphabetic Substitution Cipher)
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+def decode_vigenere(vigenere_msg, key, LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+    translated = []
+    keyIndex = 0
+    key = key.upper()
+
+    for symbol in vigenere_msg:
+        num = LETTERS.find(symbol.upper())
+        if num != -1:
+            num -= LETTERS.find(key[keyIndex])
+
+            num %= len(LETTERS)
+
+            if symbol.isupper():
+                translated.append(LETTERS[num])
+            elif symbol.islower():
+                translated.append(LETTERS[num].lower())
+
+            keyIndex += 1
+            if keyIndex == len(key):
+                keyIndex = 0
+        else:
+            translated.append(symbol)
+
+    return ''.join(translated)
+
 
 def vigenere_decoder():
     check_call(["clear"])
@@ -52,8 +71,19 @@ def vigenere_decoder():
         elif vigenere_msg == "0":
             sys.exit()
         else:
-            text_msg = decode_vigenere(vigenere_msg, key)
-            succesprint ("Mesajınız dönüştürüldü.")
+            colorprint("info", "Alfabeyi belirlemek ister misiniz? E/H")
+            colorprint("warn", "Ön tanımlı olarak ABCDEFGHIJKLMNOPQRSTUVWXYZ kullanılacak.")
+            choice = raw_input(
+                "Axion TERMINAL(" + Style.BRIGHT + Fore.CYAN + "/crypto/vigenere_decoder" + Style.RESET_ALL + ")-->")
+            if choice == 'E':
+                colorprint("warn", "Kullanmak istediğiniz alfabeyi girin.")
+                LETTERS = raw_input(
+                    "Axion TERMINAL(" + Style.BRIGHT + Fore.CYAN + "/crypto/vigenere_decoder" + Style.RESET_ALL + ")-->")
+                text_msg = decode_vigenere(vigenere_msg, key, LETTERS)
+
+            else:
+                text_msg = decode_vigenere(vigenere_msg, key)
+            colorprint("info","Mesajınız dönüştürüldü.")
             print ("Mesaj:\n--> %s" %text_msg)
 
         colorprint("info", "Başka bir mesaj dönüştürmek ister misiniz? E/H")
