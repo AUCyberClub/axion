1207ae3b1d7c904193b8bed290c989bd62811172
Behçet Şentürk <behcet.senturk@protonmail.com>
2018-04-20 00:07:37 +0300

    Update Installiation.sh

--- Installiation.sh
+++ Installiation.sh
@@ -5,8 +5,8 @@ git clone https://github.com/AUCyberClub/axion.git
 cd axion
 sudo pip install -r requirements.txt
 path=$(pwd | grep axion)
-echo "#!/bin/bash" >>axion
-echo "cd $path"
+echo "#!/bin/bash" >> axion
+echo "cd $path" >> axion
 echo "python $path/axion.py" >> axion
 sudo chmod +x axion
 sudo mv axion /usr/bin
