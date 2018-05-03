f82085016e35b49a34e0bd0a7e13fe9abbf97191
Behçet Şentürk <behcet.senturk@protonmail.com>
2018-05-04 00:50:27 +0300

    Update Installiation.sh

--- Installiation.sh
+++ Installiation.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 
+clear
+
+echo " ____________________________________________ "
+echo "|****WELCOME TO AUCC AXION INSTALLIATION*****|"
+echo "|____________________________________________|"
+echo ""
+echo "Axion is a simple toolkit that contains useful tools and capable of control the I/Os of those tools to make it easier your life in CTFs."
+echo ""
+echo "This program is under GPL-3 LICENSE"
+echo ""
+echo "https://github.com/AUCyberClub/axion"
+echo ""
+echo "Axion uses or controls following tools or packages--->"
+echo ""
+echo "git          binwalk      exiftool"
+echo "volatility   python-pip   python-pexpect"
+echo "wget         zbar-tools   pdf-parser"
+echo "john-jumbo"
+echo ""
+echo "Axion uses following external python libraries--->"
+echo ""
+echo "setuptools colorama hashid pybase64"
+echo ""
+echo "_______________________________________________________"
+echo ""
+echo "Choose a language to continue the installation"
+echo ""
+echo "'T' for Turkish and 'E' for English --->"
+
+read lang
+
+if [ "$lang" = "E" ]; then
+    git clone https://github.com/AUCyberClub/axion.git
+elif [ "$lang" = "T" ]; then
+    git clone https://github.com/AUCyberClub/axion-tr
+else
+    echo "Wrong input !!"
+    exit 1
+fi
+
 sudo apt-get install git binwalk exiftool volatility python-pip python-pexpect wget zbar-tools
-git clone https://github.com/AUCyberClub/axion.git
 cd axion
 sudo pip install -r requirements.txt
 path=$(pwd | grep axion)
